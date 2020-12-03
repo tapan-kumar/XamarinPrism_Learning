2692340dd59a1b836b441c7996bc4a294c6ceb26
Tapan Kumar <tapan11144@gmail.com>
2020-12-03 16:02:13 +0530

    commit

---
+++ DoToo/DoToo/DoToo/Resolver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+
+namespace DoToo
+{
+   public static class Resolver
+    {
+        private static IContainer _container;
+        
+
+        public static void Initialize(IContainer Container)
+        {
+            _container = Container;
+            
+
+        }
+        public static T Resolve<T>()
+        {
+            return _container.Resolve<T>();
+        }
+    }
+}

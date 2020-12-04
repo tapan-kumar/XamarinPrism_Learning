e4ef032babc0b008b313855c88176b53c60d17f3
Tapan Kumar <tapan11144@gmail.com>
2020-12-04 11:19:42 +0530

    Contact App with Listview created

---
+++ Contacts/Contacts/Contacts/Contacts.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Contacts
+{
+    public class Contacts
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public long CellNumber { get; set; }
+        //public Image DisplayPicture { get; set; }
+    }
+}
---
+++ Contacts/Contacts/Contacts/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Contacts
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var List = new List<Contacts>()
+            {
+                new Contacts
+            {
+                FirstName="Sawan",
+            LastName="Bundelkhandi",
+            CellNumber=9827109369,
+
+            },
+            new Contacts
+            {
+            FirstName="Tapan",
+            LastName="Bundelkhandi",
+            CellNumber=8103943917}};
+            list.ItemsSource = List;
+        }
+        
+    }
+}
